8fa11add54d9c38a3fa4f740311e2de73f70f6f7
Ryota <igorbenhurcampos@gmail.com>
2023-10-06 00:37:33 -0300

    game

--- main2.py
+++ main2.py
@@ -75,8 +75,8 @@ with st.sidebar:
         if 'df' in locals():
             winrate = ((pdl["wins"]) / (pdl["wins"] + pdl["losses"])) * 100
             a = pdl[["wins", 'losses']]
-            cores = {'wins': 'red', 'losses': 'lightblue'}
-            fig_kind = px.pie(a, names=a.columns, values=a.values[0], color=['blue', 'red'],
+            cores = {'wins': '#48D1CC', 'losses': '#90EE90'}
+            fig_kind = px.pie(a, names=a.columns, values=a.values[0], color=['#48D1CC', '#90EE90'],
                               title=f'Soloq - Win Rate em {a.values[0].sum()} partidas')
             st.plotly_chart(fig_kind, use_container_width=True)
             st.write(f'Tier: {pdl["tier"].iloc[0]} {pdl["rank"].iloc[0]} - PDL: {pdl["leaguePoints"].iloc[0]}')
